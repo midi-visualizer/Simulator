3800b09b5f1ab705172ae577b9119cd39f746cf6
Sebastian Lindberg <seb.lindberg@gmail.com>
2018-02-18 12:16:53 +0100

    Migrated changes from the main client repo

---
+++ lib/midi_visualizer/interface/simulator.rb
@@ -0,0 +1,15 @@
+class MIDIVisualizer::Interface::Simulator
+  require 'ruby2d'
+end
+
+require 'midi_visualizer/interface/simulator/light'
+require 'midi_visualizer/interface/simulator/light_column'
+require 'midi_visualizer/interface/simulator/light_grid'
+
+module MIDIVisualizer
+  module Interface
+    class Simulator
+      
+    end
+  end
+end
