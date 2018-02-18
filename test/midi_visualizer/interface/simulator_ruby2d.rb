5d8f03f0789ce554974cb714a7480bf662547caa
Sebastian Lindberg <seb.lindberg@gmail.com>
2018-02-18 12:56:55 +0100

    Removed old test (not really a test) and added a new

--- test/midi_visualizer/interface/simulator_ruby2d.rb
+++
@@ -1,9 +0,0 @@
-require 'color'
-require 'test_helper'
-
-interface = MIDIVisualizer::Interface::Simulator.new(6, 8)
-colors    = Array.new(6*8) { ::Color::RGB.new rand, rand, rand, 1.0 }
-
-interface.open do
-  colors
-end
